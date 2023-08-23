19bc94b3578e6ea858b8c8302aa701ce3bfca20b
WIllianMartins2018 <willian.martins01@hotmail.com>
2023-08-22 21:32:53 -0300

    Corrupção de Código
    22/08/2023

---
+++ PaymentContext/PaymentContext.Tests/Entities/StudentTests.cs
@@ -0,0 +1,16 @@
+using PaymentContext.Domain.Entities;
+
+namespace PaymentContext.Tests;
+
+[TestClass]
+public class StudentTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        var subscription = new Subscription(null);
+        var student = new Student("Willian", "Martins", "000", "estudos@gmail.com");
+        student.AddSubscription(subscription);
+
+    }
+}
